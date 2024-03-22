d31d0793b519fe4f3cd7676309552b5e2d78b86d
FrinkoTay <francis.taylor20027@gmail.com>
2024-03-22 10:32:44 +0000

    add lots of tutorial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { insertCoin } from 'playroomkit'
+import { GameEngineProvider } from './context/useGameEngine'
 import App from './App'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+insertCoin().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <GameEngineProvider>
+        <App />
+      </GameEngineProvider>
+    </React.StrictMode>,
 )
+})
